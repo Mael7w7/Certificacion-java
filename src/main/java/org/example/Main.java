d1756c1eaa15ef98ff8864e833b971348efee156
Mael7w7 <ange_lito2002@hotmail.com>
2025-11-11 01:49:54 -0500

    ejercicio terminado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,13 +4,22 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-    Cliente cliente = new Cliente();
-    cliente.setNombre("Angel Hidalgo Curo");
-    cliente.setDni("73635273");
-    CuentaBancaria cuenta = new CuentaBancaria(10.0,cliente);
-    cuenta.depositar(100.0);
-    cuenta.retirar(80.0);
-        System.out.println(cuenta.consultarSaldo());
+        Empleado empleado = new Empleado();
+        empleado.setNombre("Angel Hidalgo Curo");
+        empleado.setSalario(1000.00);
+        empleado.setPuesto("Empleado");
+        System.out.println(empleado);
+        System.out.println("Sueldo total : "+empleado.aumentarSalario(10));
+
+        System.out.println("--------------------------------------------------------------------");
+        Gerente gerente = new Gerente();
+        gerente.setNombre("Camila ramirez");
+        gerente.setSalario(2000.00);
+        gerente.setPuesto("Gerente");
+        gerente.setBono(300.00);
+        System.out.println(gerente);
+        System.out.println("Sueldo total " +gerente.calcularSalario());
+
 
     }
 }
