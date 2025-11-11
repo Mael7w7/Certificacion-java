7849a25da3bedca03d351f8d3be27d45135b44a4
Mael7w7 <ange_lito2002@hotmail.com>
2025-11-10 20:56:31 -0500

    ejercicio terminado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,32 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Alumno alumno = new Alumno() ;
 
-    Producto pro = new Producto();
-    pro.setNombre("Angel Hidalgo");
-    pro.setStock(20);
-    pro.setPrecio("320");
-    metodos metodos = new metodos(pro);
+        alumno.setNombre("Angel Hidalgo Curo");
+        alumno.setCarrera("Ingenieria de sistemas");
+        List<Double> listaNotas = new ArrayList<Double>();
 
-    System.out.println("-------Producto--------");
+        listaNotas.add(12.0);
+        listaNotas.add(15.8);
+        listaNotas.add(12.9);
+        listaNotas.add(11.6);
+        alumno.setListaNotas(listaNotas);
+
+        Metodos metodos = new Metodos(alumno);
+
+        metodos.AgregarNotas(10);
         metodos.mostrarInfo();
-        metodos.vender(30);
-        metodos.reponer(9);
-        metodos.mostrarInfo();
+        metodos.CalcularNotas();
+        metodos.notaFinal();
+
+
 
     }
 }
