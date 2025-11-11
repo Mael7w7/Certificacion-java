9277bf8c609e1e6d511fa32a3290854216eec68b
Mael7w7 <ange_lito2002@hotmail.com>
2025-11-11 02:29:29 -0500

    terminado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,22 +4,51 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Empleado empleado = new Empleado();
-        empleado.setNombre("Angel Hidalgo Curo");
-        empleado.setSalario(1000.00);
-        empleado.setPuesto("Empleado");
-        System.out.println(empleado);
-        System.out.println("Sueldo total : "+empleado.aumentarSalario(10));
-
-        System.out.println("--------------------------------------------------------------------");
-        Gerente gerente = new Gerente();
-        gerente.setNombre("Camila ramirez");
-        gerente.setSalario(2000.00);
-        gerente.setPuesto("Gerente");
-        gerente.setBono(300.00);
-        System.out.println(gerente);
-        System.out.println("Sueldo total " +gerente.calcularSalario());
 
 
+        int numero[] = { 1,2,3,4,5,6,7,8,9,10};
+        for(int n : numero) {
+            System.out.println(n);
+        }
+
+        esMenor(numero);
+        esMayor(numero);
+        promedio(numero);
+
+
+    }
+
+    public static void esMayor( int[] numero){
+        int num =numero[0];
+        for(int i = 0; i < numero.length; i++) {
+
+            if (num <=numero[i]) {
+                num=numero[i];
+            }
+
+        }
+        System.out.println("El numero mayor es " + num);
+    }
+
+    public static void esMenor( int[] numero){
+        int num=numero[0];
+        for(int i = 0; i < numero.length; i++) {
+
+            if (num >=numero[i]) {
+                num=numero[i];
+            }
+
+
+        }
+        System.out.println("El numero menor es " + num);
+    }
+
+    public static void promedio(int[] numero){
+        int acumulador=0;
+        for (var i = 0; i < numero.length; i++) {
+            acumulador += numero[i];
+
+        }
+        System.out.println("El promedio es :  " +(double)acumulador/ numero.length);
     }
 }
