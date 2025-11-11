4148a5d06660ed3a1f6692d998d801768afbed47
Mael7w7 <ange_lito2002@hotmail.com>
2025-11-10 20:07:22 -0500

    primer commit

--- src/main/java/org/example/Producto.java
+++ src/main/java/org/example/Producto.java
@@ -1,4 +1,41 @@
 package org.example;
 
 public class Producto {
+    private String nombre;
+    private  String precio;
+    private  Integer stock;
+
+
+    public Producto() {
+
+    }
+    public Producto(String precio, Integer stock, String nombre) {
+        this.precio = precio;
+        this.stock = stock;
+        this.nombre = nombre;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
--- src/main/java/org/example/metodos.java
+++ src/main/java/org/example/metodos.java
@@ -1,4 +1,40 @@
 package org.example;
 
 public class metodos {
+
+    Producto pro = new Producto();
+    public void vender(int cantidad){
+        if(pro.getStock()>cantidad){
+            int total = pro.getStock() - cantidad;
+            pro.setStock(total);
+            System.out.println("El stock es "+pro.getStock());
+        }else {
+            System.out.println("La cantidad sobrepasa el valor del stock");
+        }
+    }
+
+    public void reponer(int cantidad){
+
+        int total = pro.getStock() + cantidad;
+        pro.setStock(total);
+        System.out.println("Se agrego productos nuevos stock : " +total);
+    }
+
+    public void mostrarInfo(){
+
+        System.out.println("El nombre del producto es :  "+pro.getNombre());
+        System.out.println("El precio del producto es : " +pro.getPrecio());
+        System.out.println("El stock del producto es : "+pro.getStock());
+
+    }
+
+    public metodos(Producto pro) {
+        this.pro = pro;
+    }
+    public metodos() {
+
+    }
+
+
+
 }
